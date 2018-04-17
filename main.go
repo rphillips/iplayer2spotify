d53af237aecb88a0b9865a904fad69694013c3cf
Ryan Phillips <rphillips@redhat.com>
2018-04-17 17:27:49 -0500

    add template and CLI support

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"flag"
+	"bytes"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/anaskhan96/soup"
 	"github.com/rphillips/iplayer2spotify/secrets"
+	"github.com/rphillips/iplayer2spotify/version"
 	"github.com/zmb3/spotify"
+	"gopkg.in/urfave/cli.v1"
 )
 
-const redirectURI = "http://localhost:8080/callback"
-const maxSongsOnCreate = 100
+const defaultDateFormat = "20060102"
 const maxSearchRetries = 5
+const maxSongsOnCreate = 100
+const redirectURI = "http://localhost:8080/callback"
 
 var (
 	auth  = spotify.NewAuthenticator(redirectURI, spotify.ScopePlaylistModifyPrivate)
@@ -88,7 +93,26 @@ func createPlaylist(username string, client *spotify.Client, title string, songI
 	return nil
 }
 
-func searchForSpotifyTracks(client *spotify.Client, artistSongs []string, isCleanOnly bool) ([]spotify.ID, error) {
+func createPlaylistTitle(opts *Options) (string, error) {
+	tmplData := struct {
+		Now       string
+		CleanOnly bool
+	}{
+		time.Now().Format(opts.DateFormat),
+		opts.CleanOnly,
+	}
+	tmpl, err := template.New("playlist_title").Parse(opts.PlaylistNameTmpl)
+	if err != nil {
+		return "", err
+	}
+	var playlistTitle bytes.Buffer
+	if err := tmpl.Execute(&playlistTitle, tmplData); err != nil {
+		return "", err
+	}
+	return playlistTitle.String(), nil
+}
+
+func searchForSpotifyTracks(opts *Options, client *spotify.Client, artistSongs []string) ([]spotify.ID, error) {
 	songIDs := make([]spotify.ID, 0)
 	for _, searchData := range artistSongs {
 		log.Printf("Searching for %v\n", searchData)
@@ -104,7 +128,7 @@ func searchForSpotifyTracks(client *spotify.Client, artistSongs []string, isClea
 				return nil
 			}
 			if len(results.Tracks.Tracks) > 0 {
-				if isCleanOnly == true && results.Tracks.Tracks[0].Explicit {
+				if opts.CleanOnly == true && results.Tracks.Tracks[0].Explicit {
 					return nil
 				}
 				songIDs = append(songIDs, results.Tracks.Tracks[0].ID)
@@ -115,35 +139,46 @@ func searchForSpotifyTracks(client *spotify.Client, artistSongs []string, isClea
 	return songIDs, nil
 }
 
-func main() {
-	var showURL = flag.String("show-url", "", "url of show")
-	var playlistName = flag.String("playlist-name", "", "playlist name")
-	var isCleanOnly = flag.Bool("clean", false, "suppress explicit tracks")
+type Options struct {
+	ShowURL          string
+	PlaylistNameTmpl string
+	CleanOnly        bool
+	DateFormat       string
+}
 
-	flag.Parse()
-
-	envStr := os.Getenv("SPOTIFY_ID")
-	if envStr != "" {
-		secrets.ClientID = envStr
+func run(c *cli.Context) error {
+	options := &Options{
+		ShowURL:          c.String("show-url"),
+		PlaylistNameTmpl: c.String("playlist-name"),
+		CleanOnly:        c.Bool("clean"),
+		DateFormat:       c.String("date-format"),
 	}
-	envStr = os.Getenv("SPOTIFY_SECRET")
-	if envStr != "" {
-		secrets.SecretKey = envStr
+
+	if s := c.String("spotify-id"); s != "" {
+		secrets.ClientID = s
+	}
+	if s := c.String("spotify-secret"); s != "" {
+		secrets.SecretKey = s
 	}
 	if secrets.ClientID == "" {
-		log.Fatalf("SPOTIFY_ID not set")
+		return errors.New("SPOTIFY_ID not set")
 	}
 	if secrets.SecretKey == "" {
-		log.Fatalf("SPOTIFY_SECRET not set")
+		return errors.New("SPOTIFY_SECRET not set")
 	}
-	if *showURL == "" {
-		log.Fatalf("Invalid URL")
+	if options.ShowURL == "" {
+		return errors.New("Invalid URL")
 	}
-	if *playlistName == "" {
-		log.Fatalf("Invalid Playlist name")
+	if options.PlaylistNameTmpl == "" {
+		return errors.New("Invalid Playlist Template")
+	}
+	// Validate playlist template name
+	playlistTitle, err := createPlaylistTitle(options)
+	if err != nil {
+		return err
 	}
 
-	endpoint := *showURL + "/segments.inc"
+	endpoint := options.ShowURL + "/segments.inc"
 	log.Printf("Using Show URL %v\n", endpoint)
 
 	// Setup auth callback
@@ -162,26 +197,69 @@ func main() {
 	client := <-ch
 	client.AutoRetry = true
 
+	// Fetch current user
 	user, err := client.CurrentUser()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	// Fetch Segments
 	doc := fetchProgramSegments(endpoint)
 	artistSongs := parseSegments(string(doc))
-	songIDs, err := searchForSpotifyTracks(client, artistSongs, *isCleanOnly)
+	songIDs, err := searchForSpotifyTracks(options, client, artistSongs)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	// Format Playlist
-	now := time.Now()
-	nowFormatted := fmt.Sprintf("%02d%02d%02d", now.Year(), now.Month(), now.Day())
-	playlistTitle := fmt.Sprintf("%s - %s", *playlistName, nowFormatted)
 	if err := createPlaylist(user.ID, client, playlistTitle, songIDs...); err != nil {
+		return err
+	}
+	log.Printf("Created playlist: %s", playlistTitle)
+	return nil
+}
+
+func main() {
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Printf("%s\n", c.App.Version)
+	}
+	app := cli.NewApp()
+	app.Name = "iplayer2spotify"
+	app.Usage = "iplayer (BBC) playlist to Spotify converter"
+	app.Action = run
+	app.Version = version.Version
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "clean",
+			Usage: "suppress explicit tracks",
+		},
+		cli.StringFlag{
+			Name:  "playlist-name",
+			Usage: "playlist name, allows for go template (Supported Variables: {{ Now }} {{ CleanOnly }}",
+		},
+		cli.StringFlag{
+			Name:   "spotify-id",
+			Usage:  "Developer ID",
+			EnvVar: "SPOTIFY_ID",
+			Hidden: true,
+		},
+		cli.StringFlag{
+			Name:   "spotify-secret",
+			Usage:  "Developer Secret",
+			EnvVar: "SPOTIFY_SECRET",
+			Hidden: true,
+		},
+		cli.StringFlag{
+			Name:  "show-url",
+			Usage: "url of show",
+		},
+		cli.StringFlag{
+			Name:  "date-format",
+			Usage: "date format to use",
+			Value: defaultDateFormat,
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-
-	log.Printf("Created playlist: %s", playlistTitle)
 }
